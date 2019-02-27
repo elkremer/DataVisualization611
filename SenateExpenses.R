aa2236b8df41830fac02e5570fe8c2d4426fe13f
Peter Kremer <elkremer@yahoo.com>
2019-02-26 23:14:18 -0500

    Merged Renee's version with mine
    Also tweaked a few items.
    Removed foolish bug where if the file existed, it wouldn't create the data, which is a problem when you have previously downloaded the file, but have removed the variables from the session.
    Added scatterplot and other small changes.

--- SenateExpenses.R
+++ SenateExpenses.R
@@ -36,55 +36,53 @@ prepareExpenseData <- function(exData) {
   exData
 }
 
-
-#Function to get the data and create the choropleth
 createSenatorChoroplethMap <- function(expenses) {
   #Libraries required for choropleth map
   require(rgdal)
   library(ggsn)
   require(plyr)
   
-  #choropleth data
-  stateEx <- aggregate(amount~State, expenses, sum)
+  us <- map_data("state")
+  exp <- aggregate(amount~State, expenses, sum)
+  exp$region <- tolower(exp$State)
+  arr <- select(exp, region, amount)
+  
+  ggp <- ggplot() +geom_map(data=us, map=us,
+                          aes(x=long, y=lat, map_id=region),
+                          fill="#ffffff", color="#ffffff", size=0.15)
+  
+  ggp <- ggp + geom_map(data=arr, map=us,
+                      aes(fill=amount, map_id=region),
+                      color="#ffffff", size=0.15)
 
-  #Read in the shape file of the US
-  geom <- readOGR("C:/Users/Peter/Desktop/Utica/DataVisualization-611/Module8/cb_2017_us_state_20m/cb_2017_us_state_20m.shp")
-
-  #join attribute data to geometries
-  geom@data$id <- rownames(geom@data)   #Create an identifier for each census block (running number)
-  geoforti <- fortify(geom)             #Create fortify object 
-  geoforti <- join(geoforti, geom@data, by="id")    #Join geometries to fortify object
-
-  #Make it just contiguous US for better visibility
-  #Alaska, Hawaii and US territories made for a map too small to be of use
-  geoforti <- geoforti[geoforti$long < -50 & geoforti$long > -125,]
-  geoforti <- geoforti[geoforti$lat > 25 & geoforti$lat < 50,]
-
-  #Merge the State data data with the shape data
-  geoforti <- merge(geoforti, stateEx, by.x="NAME", by.y="State", all.x=T, a..ly=F)
-
-  #Create the map layers
-  ggp <- ggplot(data=geoforti, aes(x=long, y=lat, group=group))  #Create ggplot object
-  ggp <- ggp + geom_polygon(aes(fill=amount))  #Draw polygons
   ggp <- ggp + geom_path(color="grey")  #Draw boundaries
   ggp <- ggp + coord_equal() 
   ggp <- ggp + scale_fill_gradient(low = "#ffffcc", high = "darkred",
                                    space = "Lab", na.value = "grey50",
                                    guide = "colourbar") # specify colors
-
+  
   ggp <- ggp + labs(title="Senator Spending by State") #Add a title
   #Add scalebar 500 km
-  ggp <- ggp + scalebar(data = geoforti, dd2km = TRUE, 
+  ggp <- ggp + scalebar(data = us, dd2km = TRUE, 
                         dist=500, location="bottomright", st.size=2)
-
+  ggp <- ggp + theme(axis.ticks = element_blank())
+  ggp <- ggp + theme(panel.background = element_blank())
+  
   north2(ggp, x=.10, y=.30,scale = 0.1, symbol = 1) #Add North arrow
 }
 
-createExpensesByMonthAnimation <- function() {
-  p <- ggplot(expenses, aes(start_date, amount, color = party)) +
-    geom_point(aes(size = amount, frame = start_date, ids = State)) +
+
+createExpensesByMonthAnimation <- function(expenses) {
+  p <- ggplot(expenses, aes(start_date, amount, color = Party)) +
+    geom_point(aes(size = amount, frame = start_date, ids = State))
   
   p <- ggplotly(p)
+  
+  Sys.setenv("plotly_username"="UserNameHere")
+  Sys.setenv("plotly_api_key"="ApiKeyHere")
+  
+  chart_link = api_create(p, filename="expenses over time")
+  chart_link
 }
 
 ###############################################################################
@@ -94,18 +92,19 @@ createExpensesByMonthAnimation <- function() {
 f1 <- file.path(getwd(), "114_sdoc7_senate_data.csv", fsep = "\\")
 f2 <- file.path(getwd(), "2015senateDataStates.csv", fsep = "\\")
 
+#Don't download the file if we already have it
 if (!file.exists(f1)) {
   download.file("https://raw.githubusercontent.com/elkremer/DataVisualization611/master/114_sdoc7_senate_data.csv", f1)
-  #expenses contains the data of expenditures for the 114th senate.
-  expenses <- read.csv(file="114_sdoc7_senate_data.csv", header=TRUE, sep=",")
 }
+#expenses contains the data of expenditures for the 114th senate.
+expenses <- read.csv(file="114_sdoc7_senate_data.csv", header=TRUE, sep=",")
 
 #Don't download the file if we already have it
 if (!file.exists(f2)) {
   download.file("https://raw.githubusercontent.com/elkremer/DataVisualization611/master/2015senateDataStates.csv", f2)
-  #states is a mapping of senators to the states they serve
-  states <- read.csv(file="2015senateDataStates.csv", header=TRUE, sep=",")
 }
+#states is a mapping of senators to the states they serve
+states <- read.csv(file="2015senateDataStates.csv", header=TRUE, sep=",")
 
 expenses <- prepareExpenseData(exData = expenses)
 
@@ -114,4 +113,16 @@ senex2 <- aggregate(amount~senator_name, expenses, length)
 names(senex2) <- c("senator_name", "number_expenses")
 senex1 <- merge(senex1, senex2, all = TRUE)
 
+#Scatterplot R version
+ggplot(data = senex1) + 
+    geom_point(mapping = aes(x = amount, y = number_expenses, color = Party))
+
+#Choropleth map of US
 createSenatorChoroplethMap(expenses)
+
+#Currently fails because data is too large, must remove superfluous data and try again
+createExpensesByMonthAnimation(expenses)
+
+#Write out filtered csv files for use by Tableau
+write.csv(expenses, "filteredExpenses.csv", row.names = FALSE)
+write.csv(senex1, "senex1.csv", row.names = FALSE)
